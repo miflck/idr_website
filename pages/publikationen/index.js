9afc3d6aa788eb47aed1c79868c3577003bb8cc3
Michael Flückiger <contact@michaelflueckiger.ch>
2021-06-16 07:56:05 +0200

    publikationen fetch eingebaut

--- pages/publikationen/index.js
+++ pages/publikationen/index.js
@@ -70,9 +70,13 @@ export async function getStaticProps({locale}) {
       query: PUBLIKATIONEN, variables: {locale:locale},
     });
 
+      const res = await fetch(`https://arbor.bfh.ch/cgi/search/advanced/export_arbor_JSON.js?screen=Search&_action_export=1&output=JSON&exp=0%7C1%7C-date%2Fcreators_name%2Ftitle%7Carchive%7C-%7Cdivisions%3Adivisions%3AANY%3AEQ%3ABFH-OE--IN-0005%7C-%7Ceprint_status%3Aeprint_status%3AANY%3AEQ%3Aarchive%7Cmetadata_visibility%3Ametadata_visibility%3AANY%3AEQ%3Ashow&n=&cache=117839`)
+      const data = await res.json()
+
   return {
     props: {
-      publikationen,   
+      publikationen, 
+      data,  
       ...await serverSideTranslations(locale, ['common']),
     }, // will be passed to the page component as props
   }
