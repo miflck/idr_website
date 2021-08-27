1e43c96f60e7616ed24863992b6a33b7dd51fe1f
Michael Flückiger <contact@michaelflueckiger.ch>
2021-08-27 22:39:40 +0200

    removed components

--- components/Container/container.js
+++
@@ -1,8 +0,0 @@
-import styles from './container.module.scss'
-
-
-export default function Container({ children }) {
-    return <div className={`${styles.container} ${styles.margin_auto}`}>
-              {children}
-           </div>
-  }
