cbad5b4a562a3d838947d84a3f88939eb009dabc
Michael Flückiger <contact@michaelflueckiger.ch>
2021-09-13 18:03:37 +0200

    header trusted

--- Components/Header/header.js
+++ Components/Header/header.js
@@ -2,6 +2,8 @@ import NavMenu from '../Menu/navMenu'
 import styles from './header.module.scss'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import Head from 'next/head'
+
 
 const Header=(props)=>{
     const router = useRouter()
@@ -21,7 +23,12 @@ const Header=(props)=>{
     
     return (
         <div className={styles.headercontainer}>
+            <Head>
             <title>IDR</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta http-equiv="Content-Security-Policy"
+      content="default-src 'self'; vitals.vercel-insights.com;"></meta>
+            </Head>
             <link rel="icon" href="../favicon.ico"/>
             <div className={styles.headertitle}><Link href="/">HKB — Institute of Design Research</Link> <span className={styles.seitentitel}> — {seitentitel}</span></div>
             <NavMenu/>
