6cedcd9b576899cfc49891042fe44561b662735a
Michael Flückiger <contact@michaelflueckiger.ch>
2022-02-09 15:22:18 +0100

    merge conflict

--- Components/Header/header.js
+++ Components/Header/header.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const Header=(props)=>{
+    
     const router = useRouter()
     // console.log("router", router)
     if(router.pathname === "/") {
