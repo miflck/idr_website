4afed66c69ae02f1de4c769b3fb114d17dbf93ef
Michael Flückiger <contact@michaelflueckiger.ch>
2022-04-06 23:07:06 +0200

    lupe shrink

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ import Lupe from "../Components/Lupe/Lupe";
 import SearchTerm from "../Components/SearchTerm/SearchTerm";
 import FilterWrapper from "../Components/FilterWrapper/FilterWrapper";
 import { searchInputArray } from "../lib/helpers";
+
 import { Checkboard } from "react-color/lib/components/common";
 
 export default function Home(props) {
