90a2e5ecfa212d7516eb08a07d6d7ecc35740aa7
Michael Flückiger <contact@michaelflueckiger.ch>
2022-05-24 09:47:14 +0200

    backbutton responsive size

--- Components/SearchTermWrapper/SearchTermWrapper.js
+++ Components/SearchTermWrapper/SearchTermWrapper.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./SearchTermWrapper.module.scss";
+import Container from "../../Components/Container/container";
 
 const SearchTermWrapper = (props) => {
   const { children } = props;
   return (
     <div className={styles.root}>
-      <div className={styles.inner}>{children}</div>
+      <Container>
+        {" "}
+        <div className={styles.inner}>{children}</div>
+      </Container>
     </div>
   );
 };
