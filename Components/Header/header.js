a238a12f28367ae6d95da1bca6c36af35074483c
Michael Flückiger <contact@michaelflueckiger.ch>
2023-03-13 17:25:54 +0100

    header link

--- Components/Header/header.js
+++ Components/Header/header.js
@@ -25,7 +25,7 @@ const Header = (props) => {
         <title>IDR</title>
         <link rel="icon" href="../favicon.ico" />
         <div className={styles.headertitle}>
-          <Link href="/projekte">HKB — Institute of Design Research</Link>{" "}
+          <Link href="/">HKB — Institute of Design Research</Link>{" "}
           <span className={styles.seitentitel}> — {seitentitel}</span>
         </div>
         <NavMenu />
