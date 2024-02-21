c2cf97293d122370a07fa6e3d5519759040dd586
miflck <contact@michaelflueckiger.ch>
2024-02-21 10:29:35 +0100

    remove redirect

--- next.config.js
+++ next.config.js
@@ -3,15 +3,15 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   trailingSlash: false,
   i18n,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/projekte",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/projekte",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
   images: {
     domains: ["www.datocms-assets.com"],
     deviceSizes: [640, 768, 900, 1280],
